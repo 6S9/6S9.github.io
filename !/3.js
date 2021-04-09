11a98c1113d1219721efecbb896491cfc22e8412
宇文庶噱 <33644295+6S9@users.noreply.github.com>
2021-04-09 16:09:18 +0800

    2021年4月9日

---
+++ !/3.js
@@ -0,0 +1,6 @@
+window.onload =  function ()
+{
+    document.write("<a href='https://cn.bing.com/th?id=OHR.NorfolkPups_ZH-CN0794024596_UHD.jpg' class='js-smartphoto' data-caption='' data-id='' data-group=''><img src='https://cn.bing.com/th?id=OHR.NorfolkPups_ZH-CN0794024596_480x360.jpg' width='96'/></a>");
+
+    document.write("<a href='https://cn.bing.com/th?id=OHR.NorfolkPups_ZH-CN0794024596_UHD.jpg' class='js-smartphoto' data-caption='' data-id='' data-group=''><img src='https://cn.bing.com/th?id=OHR.NorfolkPups_ZH-CN0794024596_480x360.jpg' width='96'/></a>");
+}
