ce97a053b1cdb33144b91dff71260c828f0f248a
宇文庶噱 <33644295+6S9@users.noreply.github.com>
2021-04-11 22:21:21 +0800

    2021年4月11日

--- !/3.js
+++ !/3.js
@@ -1,6 +1,11 @@
-window.onload =  function ()
-{
-    document.write("<a href='https://cn.bing.com/th?id=OHR.NorfolkPups_ZH-CN0794024596_UHD.jpg' class='js-smartphoto' data-caption='' data-id='' data-group=''><img src='https://cn.bing.com/th?id=OHR.NorfolkPups_ZH-CN0794024596_480x360.jpg' width='96'/></a>");
+var ah1 = "<a href=";
+var pp2 = "https://slack-imgs.com/?url=";
+var u3 = "";
+var sp4 = " class='js-smartphoto' data-caption='' data-id='' data-group=''><img src='";
+var w5 = "' width='96'/></a>";
 
-    document.write("<a href='https://cn.bing.com/th?id=OHR.NorfolkPups_ZH-CN0794024596_UHD.jpg' class='js-smartphoto' data-caption='' data-id='' data-group=''><img src='https://cn.bing.com/th?id=OHR.NorfolkPups_ZH-CN0794024596_480x360.jpg' width='96'/></a>");
-}
+document.write(ah1+pp2+'https://pbs.twimg.com/media/EyqX0VeU4AI3tsc?format=jpg&name=orig'+sp4+pp2+'https://pbs.twimg.com/media/EyqX0VeU4AI3tsc?format=jpg&name=orig'+w5);
+
+document.write(ah1+pp2+'https://pbs.twimg.com/media/EyqX2GAUYAEi7px?format=jpg&name=orig'+sp4+pp2+'https://pbs.twimg.com/media/EyqX2GAUYAEi7px?format=jpg&name=orig'+w5);
+
+document.write(ah1+pp2+'https://pbs.twimg.com/media/EyoNTiGUUAMTmoP?format=jpg&name=orig'+sp4+pp2+'https://pbs.twimg.com/media/EyoNTiGUUAMTmoP?format=jpg&name=orig'+w5);
--- c/2021-04-05-format-link.md
+++ c/2021-04-05-format-link.md
@@ -11,6 +11,8 @@
 ```tip
 ```
 **FormatLink settings**{:.h3}<br>
+\ndocument.write(ah1+pp2+'{{url}}'+sc4+''+si6+'{{text}}'+sg8+sg9+i10+pp2+'{{url}}'+w12);\n
+
 \n<a href="https://slack-imgs.com/?url={{url}}" class="js-smartphoto" data-caption="{{text}}" data-id="" data-group=""><img src="https://slack-imgs.com/?url={{url}}" width="128"/></a>\n
 
 \n{{text}}<br>\n<img src="https://slack-imgs.com/?url={{url}}"><br>\n<a href="{{url}}">\n<br>{{url}}</a><hr/>\n
